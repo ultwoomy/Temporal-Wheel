6371a011afd52e70783cd1bfb66eefa845687617
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2023-11-18 21:48:55 -0600

    Added GameScene, auto. resources, & EventManager
    NOT CURRENTLY DONE W/ AUTOMATION.
    GameScene now handles increases to the spin currency.
    EventManager enables that to happen but introduces additional complexity.
    Scene scripts should inherit from GameScene.
    Automator resources are done and need to be used to continously spin the wheel. Need to have a script for it.

---
+++ Resources/Automator/Automator.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Automator
+
+@export var name: String
+@export var level: int = 1			# default value
+@export var amount: int = 1			# default value
+@export var cooldown: float = 1.0	# default value

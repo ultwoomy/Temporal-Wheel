0829975dd41a49e8ccad27dcfe96552500f64e98
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2023-12-05 02:17:20 -0600

    Shortening code w/ JSON in WheelSpaceMain.gd
    Added JSONReader class alongside a JSON file and modified updateDivisor() in WheelSpaceMain.gd to show and explain my changes.

---
+++ Resources/JSONReader/JSONReader.gd
@@ -0,0 +1,23 @@
+extends Resource
+class_name JSONReader
+
+# L.B: Here are some notes on how this exactly works. Should be simple enough to understand.
+# Load a .json file from the path provided and return the contents of it (parsed .json files are dictionaries).
+func load_json_file(filePath: String) -> Dictionary:
+	# Checks first if the file path is valid.
+	if FileAccess.file_exists(filePath):
+		# Open up the .json file and read its contents thanks to the "FileAccess" class and methods.
+		var jsonFile = FileAccess.open(filePath, FileAccess.READ)
+		
+		# Parse the contents of the .json file to be usable in Godot using the "JSON" class Godot provides.
+		var parsedResult = JSON.parse_string(jsonFile.get_as_text())
+		
+		# The parsing of the JSON file should get us a Godot dictionary.
+		if parsedResult is Dictionary:
+			return parsedResult
+		else:
+			printerr("ERROR: failed to read JSON file!")
+	else:
+		printerr("ERROR: file does not exist at path \"", filePath, "\"!")
+	# If encountered errors, return an EMPTY dictionary instead.
+	return {}

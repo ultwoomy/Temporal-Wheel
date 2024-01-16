62963ad81b65202d1765403f3b570f93f7403354
ultwoomy <88253938+ultwoomy@users.noreply.github.com>
2024-01-15 17:27:21 -0700

    Just a backup version before I try anything

--- Scripts/HellSpace/Right.gd
+++ Scripts/HellSpace/Right.gd
@@ -1,10 +1,17 @@
 extends TextureButton
 @export var backdrop : AnimatedSprite2D
+@export var zunda1 : AnimatedSprite2D
+@export var zunda2 : AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	zunda1.hide()
+	zunda2.hide()
 
 func _on_pressed():
 	hide()
 	backdrop.frame = 1
+	zunda1.show()
+	zunda1.frame = 1
+	zunda2.show()
+	zunda2.frame = 1

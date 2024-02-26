6cc80a529fc2a365876ccbb327811f53790d878b
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2024-02-25 23:19:58 -0600

    UNSTABLE: Fixes to SelectionMenu find node assignment

--- Scripts/Packsmith/PacksmithSelectionMenu.gd
+++ Scripts/Packsmith/PacksmithSelectionMenu.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 ## Components
-@onready var sigilButton01: Button = $Sigil1/Sigil01Button  # Packsmith Sigil
-@onready var sigilButton02: Button = $Sigil2/Sigil02Button  # Candle Sigil
-@onready var sigilButton03: Button = $Sigil3/Sigil03Button  # Ascension Sigil
-@onready var sigilButton04: Button = $Sigil4/Sigil04Button  # Emptiness Sigil
-@onready var sigilButton05: Button = $Sigil5/Sigil05Button  # Ritual Sigil
-@onready var sigilButton06: Button = $Sigil6/Sigil06Button  # Hell Sigil
+@onready var sigilButton01: Button = $PacksmithSigilSprite/Sigil01Button	# Packsmith Sigil
+@onready var sigilButton02: Button = $CandleSigilSprite/Sigil02Button		# Candle Sigil
+@onready var sigilButton03: Button = $AscensionSigilSprite/Sigil03Button	# Ascension Sigil
+@onready var sigilButton04: Button = $EmptinessSigilSprite/Sigil04Button	# Emptiness Sigil
+@onready var sigilButton05: Button = $RitualSigilSprite/Sigil05Button		# Ritual Sigil
+@onready var sigilButton06: Button = $HellSigilSprite/Sigil06Button			# Hell Sigil
 
 
 ## Signals
@@ -29,8 +29,9 @@ signal sigil_button_pressed(sigil: SigilData.Sigils)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for button in buttons:
-		if not button.pressed.is_connected(on_sigil_button_pressed):
-			button.pressed.connect(on_sigil_button_pressed.bind(button))
+		if button:
+			if not button.pressed.is_connected(on_sigil_button_pressed):
+				button.pressed.connect(on_sigil_button_pressed.bind(button))
 
 
 func on_sigil_button_pressed(pressedButton: Button) -> void:

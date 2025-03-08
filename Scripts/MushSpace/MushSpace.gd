53fc0cc01c86aa7fafea11489a783210b44b7109
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2025-03-07 20:00:08 -0600

    Mushspace has its own script + it shows mushbot

---
+++ Scripts/MushSpace/MushSpace.gd
@@ -0,0 +1,28 @@
+extends GameScene
+class_name MushSpace
+
+
+#@ Onready Variables
+@onready var mushbot : TextureButton = $Mushbot
+
+
+#@ Virtual Methods
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_showMushbot(Automation.contains("Mushbot"))  # If the Player has a Mushbot automator, then show the Mushbot.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+#@ Public Methods
+
+
+#@ Private Methods
+func _showMushbot(condition : bool) -> void:
+	if condition:
+		mushbot.show()
+	else:
+		mushbot.hide()

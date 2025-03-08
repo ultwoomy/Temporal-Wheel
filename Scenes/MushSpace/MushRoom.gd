518ec7efa6996e9dc8dc32233bcf15055c962f20
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2025-03-07 21:14:17 -0600

    Removed deprecated func statement (doesn't matter)
    Doesn't matter, since the statement was done elsewhere that made sense. The functionality in MushRoom.gd doesn't make sense anyways, so this script was bound to change.

--- Scenes/MushSpace/MushRoom.gd
+++ Scenes/MushSpace/MushRoom.gd
@@ -19,8 +19,6 @@ func mushbotCheck():
 func updateRots():
 	if(GVars.mushroomData.pendingRots < 0):
 		GVars.mushroomData.pendingRots = 0
-	if(GVars.sigilData.curSigilBuff == 1):
-		GVars.mushroomData.pendingRots *= 1.5
 	for n in GVars.mushroomData.current.size():
 		GVars.mushroomData.timeLeft[n] -= GVars.mushroomData.pendingRots
 		if(GVars.mushroomData.timeLeft[n] <= 0):

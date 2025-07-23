d719a47068e687727917e761f0e9aebb9edc1f8d
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2025-07-22 21:22:02 -0500

    slight code organization + sprite change

--- Scripts/ChallengeManager.gd
+++ Scripts/ChallengeManager.gd
@@ -4,18 +4,23 @@ class_name ChallengeManager
 
 #@ Export Variables
 @export var buttonContainers : Array[Control]  # (.) Maybe these valid button containers should have their own designated class.
-@export var thornsbackground : Sprite2D
+
+
 #@ Public Variables
 
 
 #@ Private Variables
 
 
+#@ Onready Variables
+@onready var thornsBackgroundSprite : Sprite2D = $ThornsBackgroundSprite
+
+
 #@ Virtual Methods
 func _ready() -> void:
 	EventManager.refresh_challenges.connect(self.refresh)
 	EventManager.disconnect_thorns.connect(self.disconnectThorns)
-	thornsbackground.hide()
+	thornsBackgroundSprite.hide()
 	await get_tree().create_timer(0.1).timeout
 	refresh()
 
@@ -26,19 +31,21 @@ func refresh():
 		if challenge is FabulousCStrat:
 			print("hh")
 			_setupFabulousChallenge(challenge)
-		
+
+
 func disconnectThorns(challenge):
-	thornsbackground.hide()
+	thornsBackgroundSprite.hide()
 	for buttonContainer in buttonContainers:
 		var fabulousChallengeComponent : FabulousCComp = buttonContainer.fabulousChallengeComponent
 		if fabulousChallengeComponent:
 			fabulousChallengeComponent.thornyButtonDamageResolved.disconnect(challenge.applyDamage)
 			buttonContainer.button.pressed.disconnect(fabulousChallengeComponent.onThornyButtonPressed)
 
+
 #@ Private Methods
 func _setupFabulousChallenge(challenge : FabulousCStrat) -> void:
-	thornsbackground.show()
-	thornsbackground.modulate = Color(1,1,1,(float(GVars.bleedstacks))/4)
+	thornsBackgroundSprite.show()
+	thornsBackgroundSprite.modulate = Color(1,1,1, (float(GVars.bleedstacks))/4)
 	for buttonContainer in buttonContainers:
 		var fabulousChallengeComponent : FabulousCComp = buttonContainer.fabulousChallengeComponent
 		if fabulousChallengeComponent:
